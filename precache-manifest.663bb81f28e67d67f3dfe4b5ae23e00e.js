cb81bfe8bd02744732ed39d5b85a44ed780b0b40
WDC <shaorin62@gmail.com>
2018-12-06 14:57:17 +0900

    Updates

---
+++ precache-manifest.663bb81f28e67d67f3dfe4b5ae23e00e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "104eab6f3b292519fad5",
+    "url": "/movie_app/static/js/main.104eab6f.chunk.js"
+  },
+  {
+    "revision": "662be294674b5dd4fb76",
+    "url": "/movie_app/static/js/1.662be294.chunk.js"
+  },
+  {
+    "revision": "104eab6f3b292519fad5",
+    "url": "/movie_app/static/css/main.ca7e429d.chunk.css"
+  },
+  {
+    "revision": "d8bc6a81fd4a9c383361a43385a202c7",
+    "url": "/movie_app/index.html"
+  }
+];
